b2b06329886faeb12b1ede589d4f5dd23112d609
Rizwan Baloch <comraderizwanbaloch@gmail.com>
2026-07-27 15:06:57 +0500

    api.js fixed

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: 'https://brew-bean.onrender.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://brew-bean.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
